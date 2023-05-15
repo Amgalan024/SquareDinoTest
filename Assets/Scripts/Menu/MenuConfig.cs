6955631cac31d0fd4b2550476f315dc81850eda7
Tokurenov Amgalan <tokurenov.a@amayasoft.uz>
2023-05-15 15:00:21 +0800

    added skyboxes asset

--- Assets/Scripts/Menu/MenuConfig.cs
+++ Assets/Scripts/Menu/MenuConfig.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Menu
 {
     public class MenuConfig : ScriptableObject
     {
+        [SerializeField] private AssetReference _gameplayScene;
     }
 }
